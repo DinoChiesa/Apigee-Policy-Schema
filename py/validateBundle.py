b005473aef9dac1b5853ad17173a8d33a134ff59
DinoChiesa <dchiesa@google.com>
2025-06-27 19:30:59 -0700

    docs: Add comment outlining --source argument enhancements

--- py/validateBundle.py
+++ py/validateBundle.py
@@ -108,6 +108,34 @@ if __name__ == "__main__":
         required=True,
         help="Directory containing XSD schema files.",
     )
+    # AI! Modify the logic slightly so that the --source argument takes either
+    # a directory (as currently implemented), OR, it takes the name of a .zip
+    # file which contains a "proxy bundle". 
+    #
+    # In case of the latter, the script should
+    #  - create a temporary directory
+    #  - unzip the zip file in that temporary directory
+    #  - verify that the structure of the unzipped contents looks something like this:
+    #
+    #     apiproxy/
+    #     apiproxy/API-PROXY-NAME-HERE.xml
+    #     apiproxy/policies/
+    #     apiproxy/policies/NAME-OF-POLICY-1.xml
+    #     apiproxy/policies/NAME-OF-POLICY-2.xml
+    #     apiproxy/policies/...
+    #     apiproxy/resources/
+    #     ...
+    #
+    # If unzip fails or if the unzipped archive does not look like that, then
+    # delete the temp directory, print an error message and exit with status 1.
+    #
+    # If the unzipped archive DOES result in a structure like that, then
+    # treat the temporary directory as the "source dir" and proceed with
+    # the logic as currently implemented.
+    #
+    # In all cases make sure to remove the temporary directory at the
+    # completion of the script. 
+
     parser.add_argument(
         "--source",
         dest="source_dir",
