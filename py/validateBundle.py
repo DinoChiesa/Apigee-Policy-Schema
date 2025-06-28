59aca8ccdfeba5c713e21f602fe2a2bc7d8d2f9a
DinoChiesa <dchiesa@google.com>
2025-06-27 17:44:01 -0700

    chore: Add Apache 2.0 license header

--- py/validateBundle.py
+++ py/validateBundle.py
@@ -1,6 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# Copyright © 2025 Google LLC.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 import argparse
 import pathlib
 import sys
@@ -41,6 +56,10 @@ def validate_with_schema(xsd_path: str, xml_path: str) -> List[str]:
     """
     errors = []
     try:
+        # AI! Keep a dictionary of XSDs by pathname, so it will be unnecessary
+        # to reload a schema if there are multiple policy files that
+        # will be validated against the same XSD. 
+        
         schema = xmlschema.XMLSchema11(xsd_path)
     except xmlschema.XMLSchemaException as e:
         errors.append(
